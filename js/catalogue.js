e92260a0d7a70defc2c5d131350a0491e5b545c1
gulrisch <fredericrichardg@gmail.com>
2026-06-19 23:21:07 +0200

    Catalogue 10 livres avec liens Amazon

---
+++ js/catalogue.js
@@ -0,0 +1,114 @@
+const catalogue = [
+  {
+    id: 1,
+    titre: "Les 48 Lois de Zarek l'Archiviste",
+    auteur: "Frédéric Richard Goulia",
+    description: "Voir, comprendre et maîtriser les règles invisibles du pouvoir — Un roman stratégique dans l'Empire d'Azyra",
+    format: "Kindle & Broché",
+    prix: "6,02 €",
+    asin: "B0H22F32KK",
+    lienAmazon: "https://www.amazon.fr/dp/B0H22F32KK",
+    image: "https://m.media-amazon.com/images/I/81example1.jpg"
+  },
+  {
+    id: 2,
+    titre: "De Rien à la Liberté",
+    auteur: "Frederic Richard Goulia",
+    description: "Comment passer de zéro à la liberté financière",
+    format: "Kindle & Broché",
+    prix: "7,76 €",
+    asin: "B0GVZM1FX9",
+    lienAmazon: "https://www.amazon.fr/dp/B0GVZM1FX9",
+    image: "https://m.media-amazon.com/images/I/81example2.jpg"
+  },
+  {
+    id: 3,
+    titre: "Le Figuier de Canaan",
+    auteur: "Frederic Goulia",
+    description: "Les Paraboles du Cœur — Un voyage entre amour, épreuves et sagesse cachée des anciens",
+    format: "Kindle & Broché",
+    prix: "5,16 €",
+    asin: "B0H22F32KK",
+    lienAmazon: "https://www.amazon.fr/s?k=Le+Figuier+de+Canaan+Goulia",
+    image: "https://m.media-amazon.com/images/I/81example3.jpg"
+  },
+  {
+    id: 4,
+    titre: "La Manifestation de l'Explosion de ma Vie Financière",
+    auteur: "Frédéric Richard Goulia",
+    description: "Le jour où nous avons décidé de changer",
+    format: "Broché",
+    prix: "18,16 €",
+    asin: "B0GV1X5JRW",
+    lienAmazon: "https://www.amazon.fr/dp/B0GV1X5JRW",
+    image: "https://m.media-amazon.com/images/I/81example4.jpg"
+  },
+  {
+    id: 5,
+    titre: "Le Dernier Pauvre de la Famille",
+    auteur: "Frédéric Richard Goulia",
+    description: "POURQUOI 90% des gens ne deviendront jamais financièrement libres",
+    format: "Broché",
+    prix: "13,48 €",
+    asin: "B0H1RTKSKG",
+    lienAmazon: "https://www.amazon.fr/dp/B0H1RTKSKG",
+    image: "https://m.media-amazon.com/images/I/81example5.jpg"
+  },
+  {
+    id: 6,
+    titre: "L'Eldorado des Ombres",
+    auteur: "Goulia Frédéric Richard",
+    description: "Ils sont partis pour vivre. Ils ont trouvé...",
+    format: "Kindle & Broché",
+    prix: "9,99 €",
+    asin: "B0H1V2WDYC",
+    lienAmazon: "https://www.amazon.fr/dp/B0H1V2WDYC",
+    image: "https://m.media-amazon.com/images/I/81example6.jpg"
+  },
+  {
+    id: 7,
+    titre: "Le Grand Secret",
+    auteur: "Frederic Goulia",
+    description: "Ce que les loges, les initiés et les sages de tous les temps ont gardé — et que l'humanité même ignore",
+    format: "Kindle & Broché",
+    prix: "5,99 €",
+    asin: "B0GVHH3KZ8",
+    lienAmazon: "https://www.amazon.fr/dp/B0GVHH3KZ8",
+    image: "https://m.media-amazon.com/images/I/81example7.jpg"
+  },
+  {
+    id: 8,
+    titre: "La Violence Humaine",
+    auteur: "Frédéric Richard Goulia",
+    description: "Comprendre les mécanismes, prévenir et se protéger",
+    format: "Kindle & Broché",
+    prix: "8,99 €",
+    asin: "B0GW643C9N",
+    lienAmazon: "https://www.amazon.fr/dp/B0GW643C9N",
+    image: "https://m.media-amazon.com/images/I/81example8.jpg"
+  },
+  {
+    id: 9,
+    titre: "L'Afrique et la Civilisation du Futur",
+    auteur: "Frédéric Richard Goulia",
+    description: "21 Fondamentaux pour Construire un Continent Nouveau",
+    format: "Broché",
+    prix: "14,99 €",
+    asin: "B0GV3L1SVL",
+    lienAmazon: "https://www.amazon.fr/s?k=Afrique+Civilisation+Futur+Goulia",
+    image: "https://m.media-amazon.com/images/I/81example9.jpg"
+  },
+  {
+    id: 10,
+    titre: "La Loi de la Semence",
+    auteur: "Frédéric Richard Goulia",
+    description: "Les principes universels de la prospérité et de la récolte",
+    format: "Kindle & Broché",
+    prix: "21,99 €",
+    asin: "B0GV1X5JRW",
+    lienAmazon: "https://www.amazon.fr/dp/B0GV1X5JRW",
+    image: "https://m.media-amazon.com/images/I/81example10.jpg"
+  }
+];
+
+export default catalogue;
